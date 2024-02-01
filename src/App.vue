792ea79a476dc885b408dae65b4f7d0920c5a978
zhangqiao <georgezhang@shein.com>
2024-02-01 15:54:29 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+    再来一次呢
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
